b7c1773a785b63e0f1c0b559a7c592a07e83662b
Joshua Orvis <jorvis@gmail.com>
2015-12-21 02:36:28 -0600

    Added Day 16, part 2

---
+++ day_16_part2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import re
+
+sues = dict()
+
+for line in open('day_16.data'):
+    line = line.rstrip()
+    m = re.match("Sue (\d+): (.+)", line)
+    if m:
+        sue_num, props = m.groups()
+        sues[sue_num] = {'valid':True}
+        for kv in props.split(", "):
+            k, v = kv.split(": ")
+            sues[sue_num][k] = int(v)
+            
+evidence = {'children':3, 'cats':7, 'samoyeds':2, 'pomeranians':3, 'akitas':0, 'vizslas':0,
+            'goldfish':5, 'trees':3, 'cars':2, 'perfumes': 1}
+    
+for sue_num in sues:
+    sue = sues[sue_num]
+    for k in sue:
+        if k == 'valid':
+            pass
+        elif k == 'cats' or k == 'trees':
+            if sue[k] <= evidence[k]:
+                sue['valid'] = False
+        elif k == 'pomeranians' or k == 'goldfish':
+            if sue[k] >= evidence[k]:
+                sue['valid'] = False
+        elif sue[k] != evidence[k]:
+            sue['valid'] = False
+
+
+    if sue['valid'] == True:
+        print("Valid Sue {1}: {0}".format(sue, sue_num))
+        
