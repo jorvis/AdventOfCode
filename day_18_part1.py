77bbf7649e8e32d4fd67a9aaf6f6cef799f08c07
Joshua Orvis <jorvis@gmail.com>
2015-12-23 07:27:46 -0600

    Removed debugging comments

--- day_18_part1.py
+++ day_18_part1.py
@@ -7,28 +7,19 @@ last_lg = list()
 
 def neighbors_on(x,y):
     on_c = 0
-    #print("DEBUG: Checking x:{0} y:{1} val:{2}".format(x,y,last_lg[x][y]))
     for i in [-1, 0, 1]:
-        if x + i < 0: 
-            #print("\tlast_lg[{0} + {1}][{2} + {3}] == {4}".format(x, i, y, None, 'out of bounds'))
-            continue
+        if x + i < 0: continue
         
         for j in [-1, 0, 1]:
-            if y + j < 0:
-                continue
-            elif i == 0 and j == 0:
-                #print("\tSkipping self, with value of {0}".format(last_lg[x + i][y + j]))
-                continue
+            if y + j < 0: continue
+            elif i == 0 and j == 0: continue
             else:
                 try:
-                    #print("\tlast_lg[{0} + {1}][{2} + {3}] == {4}".format(x, i, y, j, last_lg[x + i][y + j]))
                     if last_lg[x + i][y + j] == '#':
                         on_c += 1
                 except IndexError:
-                    #print("\tlast_lg[{0} + {1}][{2} + {3}] == {4}".format(x, i, y, j, 'out of bounds'))
                     pass
 
-    #print("DEBUG: Count of position x:{0}, y:{1} is {2}".format(x,y,on_c))
     return on_c
 
 def animate():
